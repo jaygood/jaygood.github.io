aeca6234750c629327a70bccc289ba91aa7970ea
Jon Haygood <jha@quartetfs.com>
2015-08-29 16:00:18 -0400

    Update module dependencies

--- src/back/server.js
+++ src/back/server.js
@@ -1,8 +1,5 @@
 const express = require('express');
 const http = require('http');
-const socketIo = require('socket.io');
-
-const routes = require('./routes.js');
 
 const app = express();
 const port = process.env.PORT || 6661;
